989290a94d13177f06ed5992ba3ba51cffc488cc
ksgt00901223 <810401351@qq.com>
2019-04-28 22:35:03 +0800

    ss

--- code/docs/.vuepress/config.js
+++ code/docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: '学习手册',
+  title: 'Vue懒人学习文档',
   description: '永远不要只满足于世界的表象，要敢于探寻未知的可能。',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
@@ -18,43 +18,29 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
-      // { text: '文章', link: '/article/' },
-      // { text: '类别', link: '/category' },
-      // { text: '归档', link: '/timeline' },
-      // { text: '我', link: '/poor' }
+      { text: '指南', link: '/guide/' },
       {
         text: '博客',
-        items: [
-          {
-            text: 'github',
-            link: 'https://ksgt00901223.github.io/blog/'
-          },
-          {
-            text: 'gitee',
-            link: 'https://ksgt00901223.gitee.io/blog'
-          }
-        ]
+        link: 'https://ksgt00901223.gitee.io/blog'
       },
       {
-        text: '首页',
-        items: [
-          {
-            text: 'github',
-            link: 'https://github.com/ksgt00901223/learningDocs'
-          },
-          {
-            text: 'gitee',
-            link: 'https://gitee.com/ksgt00901223/learningDocs'
-          }
-        ]
+        text: 'github',
+        link: 'https://github.com/ksgt00901223/VueLearningDocs'
       }
     ],
-    // sidebar: [
-    //   ['/article/', '文章'],
-    //   ['/category', '类别'],
-    //   ['/timeline', '归档'],
-    //   ['/poor', '我'],
-    // ],
+    sidebar: {
+      '/guide/': [
+        '基础搭建.md',
+        '模板语法.md',
+        '列表渲染.md',
+        '常用指令.md',
+        '属性方法.md',
+        '组件相关.md',
+        '路由配置.md',
+        '状态管理.md',
+        '常见问题.md'
+      ]
+    },
     activeHeaderLinks: false,
     sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     lastUpdated: '上次更新', // 文档更新时间：每个文件git最后提交的时间
